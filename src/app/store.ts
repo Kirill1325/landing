43fdf9ba8a23f1fc02531db0378870eefea96f91
Kirill Ubushaev <ubushaevkirill@gmail.com>
2024-09-25 17:20:08 +0300

    fix

--- src/app/store.ts
+++ src/app/store.ts
@@ -16,7 +16,7 @@ const store = configureStore({
 export type RootState = ReturnType<typeof rootReducer>
 export type AppDispatch = typeof store.dispatch
 
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>() // Export a hook that can be reused to resolve types
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>() 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
 
 export default store
